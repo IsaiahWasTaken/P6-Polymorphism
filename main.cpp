a3be47cd41e2553e1a8a7c9eff41087cacc69b45
IsaiahWasTaken <98197804+IsaiahWasTaken@users.noreply.github.com>
2022-03-04 20:48:07 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,4 +4,13 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
+    Rectangle r1{ 3.0, 5.0 }; 
+    Square s1{ 4.0 }; 
+    Circle c1{ 10.0 }; 
+    // printArea(r1); 
+    // printArea(s1); 
+    // printArea(c1); 
+    
+    return 0;
+
 }
