230a752bec059ed4bc7deda9cd6e02ddb1fdcabe
IsaiahWasTaken <98197804+IsaiahWasTaken@users.noreply.github.com>
2022-03-05 00:33:07 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 using namespace std;
 
+
+class Rectangle : shape{
+    double width0, height0;
+    Rectangle(double width, double height){
+
+    }
+
+};
+class Square{
+    double side0 : shape;
+    Square(double side){
+
+    }
+};
+class Circle : shape{
+    double radius0;
+    Circle(double radius){
+
+    }
+};
+class Shape{
+
+};
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-    Rectangle r1{ 3.0, 5.0 }; 
-    Square s1{ 4.0 }; 
-    Circle c1{ 10.0 }; 
-    // printArea(r1); 
-    // printArea(s1); 
-    // printArea(c1); 
-    
-    return 0;
+
+    Rectangle r1{ 3.0, 5.0 };
+    Square s1{ 4.0 };
+    Circle c1{ 10.0 };
+    // printArea(r1);
+    // printArea(s1);
+    // printArea(c1);
+
 
 }
